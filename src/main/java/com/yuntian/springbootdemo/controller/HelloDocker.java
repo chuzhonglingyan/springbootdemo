6a6a9a7063063cd9cdf501879cc6c929c80a7436
yuntian <944610721@qq.com>
2018-11-19 22:50:09 +0800

    Initial commit

---
+++ src/main/java/com/yuntian/springbootdemo/controller/HelloDocker.java
@@ -0,0 +1,14 @@
+package com.yuntian.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloDocker {
+
+    @RequestMapping("/docker")
+    public String dockerHello() {
+        return "Hello Docker World.";
+    }
+
+}
